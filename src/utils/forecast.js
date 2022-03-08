40f85b7a78a0e38423667a08a847547a140b0c20
Anirudh796 <vorugantianirudh123@gmail.com>
2022-03-08 18:30:08 +0530

    Add humidity to forecast

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,7 +1,7 @@
 const request = require('request')
 
 const forecast = (latitude,longitude, callback) => {
-    const url = 'http://api.weatherstack.com/current?access_key=cdb070b4f97e84ae6b09264704cfca3e&query=' + latitude + ',' + longitude + '&units=f'
+    const url = 'http://api.weatherstack.com/current?access_key=cdb070b4f97e84ae6b09264704cfca3e&query=' + latitude + ',' + longitude + '&units=m'
     request({ url, json:true}, (error, {body}) => {
      if(error){
         callback('Unable to connect to weather services!',undefined)
@@ -10,7 +10,8 @@ const forecast = (latitude,longitude, callback) => {
          callback('Unable to find location.Try another search',undefined)
      }
      else{
-         callback(undefined,body.current.weather_descriptions[0] + '. It is currently '+ body.current.temperature +' degrees out. ' + ' It feels like '+  body.current.feelslike + ' degrees out')
+         callback(undefined,body.current.weather_descriptions[0] + '. It is currently '+ body.current.temperature +' degrees out. ' + ' It feels like '+ 
+          body.current.feelslike + ' degrees out. The Humidity is ' + body.current.humidity + '%.')
      }
    })
  }
